1e7922203d058cc19d9d4e5540b50c7fb8fd7716
Thien <thienxuanle@yahoo.com>
2019-07-06 09:26:11 -0500

    Jenkins pipline

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 
+		System.out.println("Jenkins pipeline");
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("Hello World ....." +1);
 		}
